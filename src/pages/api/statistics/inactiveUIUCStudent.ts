9633cba2f81dd54abbfcb0a2036ff82160af6a76
jwang0306 <ace020771@gmail.com>
2021-11-06 11:43:55 -0500

    Fix inactive uiuc user query

--- src/pages/api/statistics/inactiveUIUCStudent.ts
+++ src/pages/api/statistics/inactiveUIUCStudent.ts
@@ -9,7 +9,7 @@ const inactiveUIUCStudent = async (
   const query = `
   SELECT u.id, u.email
   FROM USER u LEFT JOIN JOB_STATUS js ON u.id = js.user_id JOIN JOB j ON j.id = js.job_id \
-  WHERE (DATE(js.create_at) <= CURDATE() - interval 1 month)
+  WHERE (DATE(js.create_at) <= CURDATE() - interval 1 month) AND u.email LIKE '%@illinois.edu' \
   GROUP BY u.id \
   HAVING COUNT(*) < 5 \
   ORDER BY u.email ASC LIMIT 15;
