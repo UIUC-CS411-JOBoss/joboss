0340d27217656b5b434482d0798967158487201e
Kuan-Yin Chen <kuanyin-chen@Kuan-Yins-MacBook-Air.local>
2021-12-01 01:56:32 -0600

    fix: set google url

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-let url1 = process.env.NODE_ENV === "development"
-? "http://localhost:3000/"
-: "https://joboss.netlify.app";
-let url2 = process.env.BASE_URL
-
-export const BASE_URL = url2 || url1
-
-console.log(`BASE_URL: ${BASE_URL}`)
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/"
+    : "https://joboss-qqfbxxfkea-uc.a.run.app";
