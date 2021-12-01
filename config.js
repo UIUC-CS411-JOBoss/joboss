f1d06c6e78f2f80a5d4658e15039c540408c57dc
Kuan-Yin Chen <kuanyin-chen@Kuan-Yins-MacBook-Air.local>
2021-12-01 01:38:56 -0600

    fix: base url

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/"
-    : "https://joboss.netlify.app";
+let url1 = process.env.NODE_ENV === "development"
+? "http://localhost:3000/"
+: "https://joboss.netlify.app";
+let url2 = process.env.BASE_URL
+
+export const BASE_URL = url2 || url1
